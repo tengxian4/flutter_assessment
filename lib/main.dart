c3b8af30ffa43980904a868800eb5c0ed54bce29
tengxian4 <khawxian@gmail.com>
2022-07-20 10:58:53 +0800

    adding 5 random contacts, toggle buttons

--- lib/main.dart
+++ lib/main.dart
@@ -168,12 +168,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
 
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: !sharing?AppBar(title: Text(widget.title),
         actions: [ToggleButtons(children: <Widget>[
